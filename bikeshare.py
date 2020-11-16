9af2e28f6bbaacc8fa7cfa5de4abb46b2463e106
Maegan Porpora <maegan.porpora@shell.com>
2020-11-16 14:20:09 +0300

    Update description of user_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -142,7 +142,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users including count of gender and year of birth"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
